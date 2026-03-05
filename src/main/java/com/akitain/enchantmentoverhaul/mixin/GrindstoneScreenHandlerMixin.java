583a6d48db2e4e077e243dc2ee6741ac02e79cb6
Corentin Renard <corentinfox08@gmail.com>
2026-03-05 17:20:10 +0100

    feat: grindstone removes curses too

--- src/main/java/com/akitain/enchantmentoverhaul/mixin/GrindstoneScreenHandlerMixin.java
+++ src/main/java/com/akitain/enchantmentoverhaul/mixin/GrindstoneScreenHandlerMixin.java
@@ -1,6 +1,7 @@
 package com.akitain.enchantmentoverhaul.mixin;
 
 import com.akitain.enchantmentoverhaul.component.ModComponents;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.GrindstoneScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,6 +16,9 @@ public class GrindstoneScreenHandlerMixin {
     private void applySlotPenalty(ItemStack item, CallbackInfoReturnable<ItemStack> cir) {
         ItemStack result = cir.getReturnValue();
         if (result.isEmpty()) return;
+
+        EnchantmentHelper.apply(result, components -> components.remove(enchantment -> true));
+
         int penalty = result.getOrDefault(ModComponents.GRINDSTONE_PENALTY, 0);
         result.set(ModComponents.GRINDSTONE_PENALTY, penalty + 1);
     }
