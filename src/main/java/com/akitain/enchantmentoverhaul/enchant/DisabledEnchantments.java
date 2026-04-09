e67d3b9fd6aac38396ca781e84afc95a479e66c7
Corentin Renard <corentinfox08@gmail.com>
2026-04-09 16:24:51 +0200

    feat: re-enable Punch enchantment with Snowball reagent

--- src/main/java/com/akitain/enchantmentoverhaul/enchant/DisabledEnchantments.java
+++ src/main/java/com/akitain/enchantmentoverhaul/enchant/DisabledEnchantments.java
@@ -20,8 +20,7 @@ public class DisabledEnchantments {
             Enchantments.IMPALING,
             Enchantments.UNBREAKING,
             Enchantments.EFFICIENCY,
-            Enchantments.BANE_OF_ARTHROPODS,
-            Enchantments.PUNCH
+            Enchantments.BANE_OF_ARTHROPODS
     );
 
     public static boolean isDisabled(Holder<Enchantment> entry) {
--- src/main/java/com/akitain/enchantmentoverhaul/loot/LootTableModifier.java
+++ src/main/java/com/akitain/enchantmentoverhaul/loot/LootTableModifier.java
@@ -55,7 +55,7 @@ public class LootTableModifier {
 
             // Medium (25% chance)
             new StructureLoot("chests/simple_dungeon", 75, 12, List.of(
-                    Enchantments.KNOCKBACK, Enchantments.THORNS)),
+                    Enchantments.KNOCKBACK, Enchantments.THORNS, Enchantments.PUNCH)),
             new StructureLoot("chests/ruined_portal", 75, 12, List.of(
                     ModEnchantments.CURSE_OF_FRAGILITY, Enchantments.VANISHING_CURSE)),
             new StructureLoot("chests/underwater_ruin_small", 80, 8, List.of(
@@ -69,7 +69,7 @@ public class LootTableModifier {
             new StructureLoot("chests/abandoned_mineshaft", 75, 12, List.of(
                     Enchantments.SILK_TOUCH, Enchantments.FORTUNE)),
             new StructureLoot("chests/pillager_outpost", 75, 6, List.of(
-                    Enchantments.MULTISHOT, Enchantments.PIERCING, Enchantments.QUICK_CHARGE, Enchantments.SWEEPING_EDGE)),
+                    Enchantments.MULTISHOT, Enchantments.PIERCING, Enchantments.QUICK_CHARGE, Enchantments.SWEEPING_EDGE, Enchantments.PUNCH)),
 
             // Hard (35% chance)
             new StructureLoot("chests/buried_treasure", 65, 17, List.of(
